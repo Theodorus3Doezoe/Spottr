cf35c4d5029846948f81e68fe21e96708ef27398
Theodous3Doezoe <svendinkla@gmail.com>
2025-03-27 22:11:20 +0100

    component setup

---
+++ client/src/components/match_message/Match_message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function match_message() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+//Components
+import Header from './components/header/Header'
+
 const router = createBrowserRouter([
   {path: "/", element: <>
-
+    <Header/>
   </>},
 ])
 
